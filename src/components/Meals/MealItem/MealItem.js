a807b653f3a70e1b5450b1c2905772794609c5af
Ian F <ian.fiddeman@gmail.com>
2021-07-27 20:56:41 +0100

    mealItem added and css

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,12 @@
+const MealItem = props => { 
+    const price = `$${props.price.toFixed(2)}`;
+
+    return <li>
+        <div><h3>{props.name}</h3></div>
+        <div>{props.description}</div>
+        <div>{price}</div>
+        
+    </li>
+};
+
+export default MealItem;
