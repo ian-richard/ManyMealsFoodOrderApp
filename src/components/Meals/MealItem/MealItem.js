ef6063ce2eff8c00012988fc4f2f513982f17a85
Ian F <ian.fiddeman@gmail.com>
2021-07-29 17:04:27 +0100

    mealitem added to available mmeals component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,13 @@
+import classes from './MealItem.module.css';
+
 const MealItem = props => { 
     const price = `$${props.price.toFixed(2)}`;
 
-    return <li>
+    return <li className={classes.meal}>
         <div><h3>{props.name}</h3></div>
-        <div>{props.description}</div>
-        <div>{price}</div>
-        
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+
     </li>
 };
 
