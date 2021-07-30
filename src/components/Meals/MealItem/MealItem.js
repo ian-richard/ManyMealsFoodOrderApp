afa9ff7f337a2803fe2ca7bc59690da87bf536f3
Ian F <ian.fiddeman@gmail.com>
2021-07-30 15:06:48 +0100

    mealitemform component added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,21 @@
-import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
 
-const MealItem = props => { 
-    const price = `$${props.price.toFixed(2)}`;
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
 
-    return <li className={classes.meal}>
-        <div><h3>{props.name}</h3></div>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+      </div>
+      <div className={classes.description}>{props.description}</div>
+      <div className={classes.price}>{price}</div>
+      <div>
+          <MealItemForm/>
+      </div>
     </li>
+  );
 };
 
 export default MealItem;
