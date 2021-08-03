833fcf68386584a8d03351f325db7993c03e96a9
Ian F <ian.fiddeman@gmail.com>
2021-08-03 23:46:18 +0100

    css on mealitemForm needs tweaking

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
-import MealItemForm from "./MealItemForm";
-import classes from "./MealItem.module.css";
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -8,11 +8,11 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
       </div>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
       <div>
-          <MealItemForm/>
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,18 +2,20 @@ import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
-    return (<form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount',
-            type: 'amount',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
-
-        <button>+ Add</button>
-        
+  return (
+    <form className={classes.form}>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
     </form>
   );
 };
