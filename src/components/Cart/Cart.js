fb587168192df8eeedadf2eb1dd47f707268808f
Ian F <ian.fiddeman@gmail.com>
2021-08-06 22:59:35 +0100

    cart item near complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,27 @@
-import classes from './Cart.module.css'
+import classes from "./Cart.module.css";
 
-const Cart = props => {
-    const cartItems = [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => <li>{item.name}</li>);
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
-    return <div>
-        cartitems
-        <div></div>
-        <div></div>
+  return (
+    <div>
+      {cartitems}
+      <div>
+          <span>Total Amount</span>
+          <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+          <button className={classes['button--alt']}>Close</button>
+          <button className={classes.button}>Order</button>
+      </div>
     </div>
+  );
 };
 
 export default Cart;
