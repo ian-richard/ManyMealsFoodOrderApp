18c5487f09422c8f4df281b68753cd3680cfa576
Ian F <ian.fiddeman@gmail.com>
2021-08-08 23:05:38 +0100

    cart added with added dummy data

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,9 +7,9 @@ import classes from './Header.module.css';
 
 const Header = props => {
     return <Fragment>
-        <header className={classes.header}>
+        <header className={classes.header} onClick={props.onClick}>
             <h1>ManyMeals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='a table full of yummy food' />
