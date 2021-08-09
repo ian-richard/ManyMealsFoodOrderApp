847137a8a925f32852acdf6eca5b8e2612bfbe3e
Ian F <ian.fiddeman@gmail.com>
2021-08-10 00:07:03 +0100

    cart provider component added

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,7 +1,7 @@
 import { createContext } from 'istanbul-lib-report';
 import React from 'react';
 
-const CardContext = React.createContext({
+const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
     addItem: (item) => {},
