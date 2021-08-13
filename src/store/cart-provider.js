fa5f46e9262823637e7d31fc3f676787eb9465d6
Ian F <ian.fiddeman@gmail.com>
2021-08-13 17:21:31 +0100

    cart now totalling

--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -10,6 +10,12 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
         const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+
 
     }
     return defaultCartState;
